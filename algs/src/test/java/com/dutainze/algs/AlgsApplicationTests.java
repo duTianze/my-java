7906276a7c6604d3985729704c672d4fcc790151
dutianze <dutianzer@gmail.com>
2022-03-11 23:15:24 +0800

    init

---
+++ algs/src/test/java/com/dutainze/algs/AlgsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dutainze.algs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
