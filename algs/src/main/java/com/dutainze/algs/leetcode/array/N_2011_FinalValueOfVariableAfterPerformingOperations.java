9607b9bf71ee74f434ebb444b880810a2b566373
dutianze <dutianzer@gmail.com>
2022-03-18 00:57:15 +0800

    N_2011_FinalValueOfVariableAfterPerformingOperations

--- algs/src/main/java/com/dutainze/algs/leetcode/array/N_2011_FinalValueOfVariableAfterPerformingOperations.java
+++ algs/src/main/java/com/dutainze/algs/leetcode/array/N_2011_FinalValueOfVariableAfterPerformingOperations.java
@@ -7,6 +7,14 @@ package com.dutainze.algs.leetcode.array;
 public class N_2011_FinalValueOfVariableAfterPerformingOperations {
 
     public int finalValueAfterOperations(String[] operations) {
-return 1;
+        int num = 0;
+        for (String operation : operations) {
+            if (operation.contains("++")) {
+                num ++;
+            } else if (operation.contains("--")) {
+                num --;
+            }
+        }
+        return num;
     }
 }
