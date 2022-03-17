6e46ab20f0c4f946bf9676269ea356eb896b80f2
dutianze <dutianzer@gmail.com>
2022-03-17 23:56:49 +0800

    a

--- algs/src/main/java/com/dutainze/algs/leetcode/array/N_2011_FinalValueOfVariableAfterPerformingOperations.java
+++ algs/src/main/java/com/dutainze/algs/leetcode/array/N_2011_FinalValueOfVariableAfterPerformingOperations.java
@@ -5,4 +5,8 @@ package com.dutainze.algs.leetcode.array;
  * @date 2022/3/17
  */
 public class N_2011_FinalValueOfVariableAfterPerformingOperations {
+
+    public int finalValueAfterOperations(String[] operations) {
+return 1;
+    }
 }
