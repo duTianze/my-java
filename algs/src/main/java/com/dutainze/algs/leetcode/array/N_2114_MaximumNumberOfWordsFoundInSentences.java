0bcfb727b3c7cee26a2c667b2e3ed78b09ed02eb
dutianze <dutianzer@gmail.com>
2022-03-19 23:59:13 +0800

    a

--- algs/src/main/java/com/dutainze/algs/leetcode/array/N_2114_MaximumNumberOfWordsFoundInSentences.java
+++ algs/src/main/java/com/dutainze/algs/leetcode/array/N_2114_MaximumNumberOfWordsFoundInSentences.java
@@ -1,8 +1,17 @@
 package com.dutainze.algs.leetcode.array;
 
 /**
+ * <a href="https://leetcode.com/problems/maximum-number-of-words-found-in-sentences/">2114. Maximum Number of Words Found in Sentences</a>
+ * <h2>Easy</h2>
+ * <pre>
+ *
+ * </pre>
  * @author dutianze
  * @date 2022/3/19
  */
-public interface N_2114_MaximumNumberOfWordsFoundInSentences {
+public class N_2114_MaximumNumberOfWordsFoundInSentences {
+
+    public int mostWordsFound(String[] sentences) {
+
+    }
 }
