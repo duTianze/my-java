b272f84756f0da7bf079a3a90ebf1e3f7dcc9d9f
dutianze <dutianzer@gmail.com>
2022-03-22 00:02:07 +0800

    a

---
+++ algs/src/main/java/com/dutainze/algs/leetcode/array/N_1476_SubrectangleQueries.java
@@ -0,0 +1,20 @@
+package com.dutainze.algs.leetcode.array;
+
+/**
+ * @author dutianze
+ * @date 2022/3/21
+ */
+public class N_1476_SubrectangleQueries {
+
+    public N_1476_SubrectangleQueries(int[][] rectangle) {
+
+    }
+
+    public void updateSubrectangle(int row1, int col1, int row2, int col2, int newValue) {
+
+    }
+
+    public int getValue(int row, int col) {
+
+    }
+}
