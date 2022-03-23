a9effb05a05c26e0cdc815b6d1da216d2567690a
dutianze <dutianzer@gmail.com>
2022-03-23 23:48:33 +0800

    a

--- algs/src/main/java/com/dutainze/algs/leetcode/array/N_1476_SubrectangleQueries.java
+++ algs/src/main/java/com/dutainze/algs/leetcode/array/N_1476_SubrectangleQueries.java
@@ -1,20 +1,36 @@
 package com.dutainze.algs.leetcode.array;
 
+import com.dutainze.util.PrettyPrinter;
+
+import java.util.Arrays;
+
 /**
  * @author dutianze
  * @date 2022/3/21
  */
 public class N_1476_SubrectangleQueries {
 
+    private final int[][] rectangle;
+
     public N_1476_SubrectangleQueries(int[][] rectangle) {
-1111
+        this.rectangle = rectangle;
     }
 
     public void updateSubrectangle(int row1, int col1, int row2, int col2, int newValue) {
-
+        for (int i = row1; i <= row2; i++) {
+            for (int j = col1; j <= col2; j++) {
+                rectangle[i][j] = newValue;
+            }
+        }
     }
 
     public int getValue(int row, int col) {
+        return this.rectangle[row][col];
+    }
 
+    @Override
+    public String toString() {
+        PrettyPrinter prettyPrinter = new PrettyPrinter(System.out);
+        return prettyPrinter.print(rectangle);
     }
 }
---
+++ algs/src/test/java/com/dutainze/algs/leetcode/array/N_1476_SubrectangleQueriesTest.java
@@ -0,0 +1,25 @@
+package com.dutainze.algs.leetcode.array;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author dutianze
+ * @date 2022/3/23
+ */
+class N_1476_SubrectangleQueriesTest {
+
+
+    @Test
+    void updateAndGetValueTest() {
+        int[][] rectangle = {{1, 1, 1}, {2, 2, 2}, {3, 3, 3}};
+        N_1476_SubrectangleQueries n_1476_subrectangleQueries = new N_1476_SubrectangleQueries(rectangle);
+        System.out.println(n_1476_subrectangleQueries);
+
+        int actual = n_1476_subrectangleQueries.getValue(1, 2);
+        Assertions.assertEquals(actual, 2);
+
+        n_1476_subrectangleQueries.updateSubrectangle(0, 0, 1, 1, 7);
+        System.out.println(n_1476_subrectangleQueries);
+    }
+}
