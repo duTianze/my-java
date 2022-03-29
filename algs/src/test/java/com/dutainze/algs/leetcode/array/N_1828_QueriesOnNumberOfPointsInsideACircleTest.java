86455e02a6f79016eec58634b8653bab232422ef
dutianze <dutianzer@gmail.com>
2022-03-29 23:54:50 +0800

    a

---
+++ algs/src/test/java/com/dutainze/algs/leetcode/array/N_1828_QueriesOnNumberOfPointsInsideACircleTest.java
@@ -0,0 +1,19 @@
+package com.dutainze.algs.leetcode.array;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author dutianze
+ * @date 2022/3/29
+ */
+@SpringBootTest
+class N_1828_QueriesOnNumberOfPointsInsideACircleTest {
+
+    @Test
+    void countPoints() {
+
+    }
+}
