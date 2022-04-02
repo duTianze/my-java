27d854bdd6c2269a4fcd7276f891d5ff0eeb93bd
dutianze <dutianzer@gmail.com>
2022-04-02 23:53:46 +0800

    N_1689_PartitioningIntoMinimumNumberOfDeciBinaryNumbers

--- algs/src/test/java/com/dutainze/algs/leetcode/string/N_1689_PartitioningIntoMinimumNumberOfDeciBinaryNumbersTest.java
+++ algs/src/test/java/com/dutainze/algs/leetcode/string/N_1689_PartitioningIntoMinimumNumberOfDeciBinaryNumbersTest.java
@@ -1,45 +1,24 @@
 package com.dutainze.algs.leetcode.string;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 /**
- * <a href="https://leetcode.com/problems/partitioning-into-minimum-number-of-deci-binary-numbers/">1689. Partitioning Into Minimum Number Of Deci-Binary Numbers</a>
- * <h2>Medium</h2>direct
- * <pre>
- * A decimal number is called deci-binary if each of its digits is either 0 or 1 without any leading zeros. For example, 101 and 1100 are deci-binary, while 112 and 3001 are not.
- *
- * Given a string n that represents a positive decimal integer, return the minimum number of positive deci-binary numbers needed so that they sum up to n.
- *
- * Example 1:
- *
- * Input: n = "32"
- * Output: 3
- * Explanation: 10 + 11 + 11 = 32
- *
- * Example 2:
- *
- * Input: n = "82734"
- * Output: 8
- *
- * Example 3:
- *
- * Input: n = "27346209830709182346"
- * Output: 9
- *
- * Constraints:
- *
- *     1 <= n.length <= 105
- *     n consists of only digits.
- *     n does not contain any leading zeros and represents a positive integer.
- * </pre>
  * @author dutianze
  * @date 2022/4/2
  */
 class N_1689_PartitioningIntoMinimumNumberOfDeciBinaryNumbersTest {
 
+    private final N_1689_PartitioningIntoMinimumNumberOfDeciBinaryNumbers solution =
+            new N_1689_PartitioningIntoMinimumNumberOfDeciBinaryNumbers();
+
     @Test
     void minPartitions() {
+        String n = "32";
+        int expected = 3;
+
+        int result = solution.minPartitions(n);
+
+        Assertions.assertEquals(3, result);
     }
 }
