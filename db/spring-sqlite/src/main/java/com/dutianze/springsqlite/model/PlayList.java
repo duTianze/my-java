6966674e83185576fff139afd2a17b495f9f2f73
dutianze <dutianzer@gmail.com>
2022-04-21 23:59:01 +0800

    fix

--- db/spring-sqlite/src/main/java/com/dutianze/springsqlite/model/PlayList.java
+++ db/spring-sqlite/src/main/java/com/dutianze/springsqlite/model/PlayList.java
@@ -1,8 +1,20 @@
 package com.dutianze.springsqlite.model;
 
+import lombok.Data;
+
+import javax.persistence.*;
+
 /**
  * @author dutianze
  * @date 2022/4/21
  */
+@Data
+@Entity
+@Table(name = "playlists")
 public class PlayList {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer playlistId;
+    private String name;
 }
--- db/spring-sqlite/src/main/java/com/dutianze/springsqlite/model/Track.java
+++ db/spring-sqlite/src/main/java/com/dutianze/springsqlite/model/Track.java
@@ -1,8 +1,28 @@
 package com.dutianze.springsqlite.model;
 
+import lombok.Data;
+
+import javax.persistence.*;
+
 /**
  * @author dutianze
  * @date 2022/4/21
  */
+@Data
+@Entity
+@Table(name = "tracks")
 public class Track {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer trackId;
+    private String name;
+    private Integer albumId;
+    private Integer mediaTypeId;
+    private Integer genreId;
+    private String composer;
+    private Integer milliseconds;
+    private Integer bytes;
+
+    private Double unitPrice;
 }
