de4c07ccf2e0b2bb1e82ea8f5d9cd5abd6149e1f
dutianze <dutianzer@gmail.com>
2022-04-30 19:23:55 +0800

    init multi module springboot project

--- base/base-web/src/main/java/com/dutianze/base/web/controller/OkController.java
+++ base/base-web/src/main/java/com/dutianze/base/web/controller/OkController.java
@@ -1,5 +1,19 @@
-package com.dutianze.base.web.controller; /**
+package com.dutianze.base.web.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
  * @author dutianze
  * @date 2022/4/30
- */public class OkController {
+ */
+@RestController
+@RequestMapping("/api/ok")
+public class OkController {
+
+    @GetMapping
+    public String ok() {
+        return "ok";
+    }
 }
