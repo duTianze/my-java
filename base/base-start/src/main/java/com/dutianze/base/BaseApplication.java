e9fecd927744ba50da65608481cb0a517f7d8afe
dutianze <dutianzer@gmail.com>
2022-04-30 19:23:35 +0800

    init multi module springboot project

---
+++ base/base-start/src/main/java/com/dutianze/base/BaseApplication.java
@@ -0,0 +1,15 @@
+package com.dutianze.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author dutianze
+ * @date 2022/4/30
+ */
+@SpringBootApplication
+public class BaseApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BaseApplication.class, args);
+    }
+}
---
+++ base/base-web/src/main/java/com/dutianze/base/web/controller/OkController.java
@@ -0,0 +1,5 @@
+package com.dutianze.base.web.controller; /**
+ * @author dutianze
+ * @date 2022/4/30
+ */public class OkController {
+}
