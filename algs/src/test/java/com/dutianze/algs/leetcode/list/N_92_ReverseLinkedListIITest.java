ee8f087cdeb88ed5b6a0f7993260c5f279f4c874
dutianze <dutianzer@gmail.com>
2022-05-11 22:15:04 +0800

    N_92_ReverseLinkedListII loop

--- algs/src/test/java/com/dutianze/algs/leetcode/list/N_92_ReverseLinkedListIITest.java
+++ algs/src/test/java/com/dutianze/algs/leetcode/list/N_92_ReverseLinkedListIITest.java
@@ -21,4 +21,15 @@ class N_92_ReverseLinkedListIITest {
 
         Assertions.assertEquals(expected.toString(), result.toString());
     }
+
+    @Test
+    void reverseBetweenLoop() {
+        ListNode resource = ListNode.with(new Integer[]{1, 2, 3, 4, 5});
+        int left = 2, right = 4;
+        ListNode expected = ListNode.with(new Integer[]{1, 4, 3, 2, 5});
+
+        ListNode result = solution.reverseBetweenLoop(resource, left, right);
+
+        Assertions.assertEquals(expected.toString(), result.toString());
+    }
 }
