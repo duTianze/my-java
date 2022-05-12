9e8c0a99cd47b8af587bec7ce22901a43953646f
dutianze <dutianzer@gmail.com>
2022-05-12 23:49:06 +0800

    N_2_AddTwoNumbers

---
+++ algs/src/main/java/com/dutianze/algs/leetcode/list/N_2_AddTwoNumbers.java
@@ -0,0 +1,33 @@
+package com.dutianze.algs.leetcode.list;
+
+/**
+ * <a href="https://leetcode.com/problems/add-two-numbers/">2. Add Two Numbers</a>
+ * <h2>Medium</h2>
+ * <pre>
+ * You are given two non-empty linked lists representing two non-negative integers.
+ * The digits are stored in reverse order, and each of their nodes contains a single digit.
+ * Add the two numbers and return the sum as a linked list.
+ *
+ * You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+ *
+ * Example 1:
+ *
+ * Input: l1 = [2,4,3], l2 = [5,6,4]
+ * Output: [7,0,8]
+ * Explanation: 342 + 465 = 807.
+ *
+ * Constraints:
+ *
+ *     The number of nodes in each linked list is in the range [1, 100].
+ *     0 <= Node.val <= 9
+ *     It is guaranteed that the list represents a number that does not have leading zeros.
+ * </pre>
+ * @author dutianze
+ * @date 2022/5/12
+ */
+public class N_2_AddTwoNumbers {
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        return null;
+    }
+}
---
+++ algs/src/test/java/com/dutianze/algs/leetcode/list/N_2_AddTwoNumbersTest.java
@@ -0,0 +1,29 @@
+package com.dutianze.algs.leetcode.list;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author dutianze
+ * @date 2022/5/12
+ */
+class N_2_AddTwoNumbersTest {
+
+    private final N_2_AddTwoNumbers solution = new N_2_AddTwoNumbers();
+
+    /**
+     * * Input: l1 = [2,4,3], l2 = [5,6,4]
+     * * Output: [7,0,8]
+     * * Explanation: 342 + 465 = 807.
+     */
+    @Test
+    void addTwoNumbers() {
+        ListNode l1 = ListNode.with(new Integer[]{2, 4, 3});
+        ListNode l2 = ListNode.with(new Integer[]{5, 6, 4});
+        ListNode expected = ListNode.with(new Integer[]{7, 0, 8});
+
+        ListNode result = solution.addTwoNumbers(l1, l2);
+
+        Assertions.assertEquals(expected.toString(), result.toString());
+    }
+}
