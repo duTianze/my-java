f65ebe1d3eb24c9f70490ce4d731df3c1c86e11d
dutianze <dutianzer@gmail.com>
2022-05-15 00:04:11 +0800

    N_11_ContainerWithMostWater

--- algs/src/main/java/com/dutianze/algs/leetcode/array/N_11_ContainerWithMostWater.java
+++ algs/src/main/java/com/dutianze/algs/leetcode/array/N_11_ContainerWithMostWater.java
@@ -33,10 +33,13 @@ package com.dutianze.algs.leetcode.array;
 public class N_11_ContainerWithMostWater {
 
     public int maxArea(int[] height) {
-        int maxArea = 0;
-        for (int i = 0; i < height.length; i++) {
-            for (int j = i + 1; j < height.length; j++) {
-                maxArea = Math.max(maxArea, Math.min(height[i], height[j]) * (j - i));
+        int maxArea = 0, left = 0, right = height.length - 1;
+        while (left < right) {
+            maxArea = Math.max(maxArea, Math.min(height[left], height[right]) * (right - left));
+            if (height[left] < height[right]) {
+                left ++;
+            } else {
+                right --;
             }
         }
         return maxArea;
