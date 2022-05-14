b60704d269abdf10849d4bc59f3f38543ed7300a
dutianze <dutianzer@gmail.com>
2022-05-14 23:43:43 +0800

    N_11_ContainerWithMostWater

---
+++ algs/src/main/java/com/dutianze/algs/leetcode/array/N_11_ContainerWithMostWater.java
@@ -0,0 +1,24 @@
+package com.dutianze.algs.leetcode.array;
+
+/**
+ * <a href="https://leetcode.com/problems/container-with-most-water/">11. Container With Most Water</a>
+ * <h2>Medium</h2>
+ * <pre>
+ * You are given an integer array height of length n.
+ * There are n vertical lines drawn such that the two endpoints of the i^th line are (i, 0) and (i, height[i]).
+ *
+ * Find two lines that together with the x-axis form a container, such that the container contains the most water.
+ *
+ * Return the maximum amount of water a container can store.
+ *
+ * Notice that you may not slant the container.
+ * </pre>
+ * @author dutianze
+ * @date 2022/5/14
+ */
+public class N_11_ContainerWithMostWater {
+
+    public int maxArea(int[] height) {
+
+    }
+}
