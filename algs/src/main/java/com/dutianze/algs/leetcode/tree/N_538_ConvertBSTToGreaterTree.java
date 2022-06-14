30f01cd01d807058cfd5727b1b36bb52db67f514
dutianze <dutianzer@gmail.com>
2022-06-14 23:58:21 +0800

    fix

---
+++ algs/src/main/java/com/dutianze/algs/leetcode/tree/N_538_ConvertBSTToGreaterTree.java
@@ -0,0 +1,9 @@
+package com.dutianze.algs.leetcode.tree;
+
+/**
+ * <a href="https://leetcode.com/problems/convert-bst-to-greater-tree/">538. Convert BST to Greater Tree</a>
+ * @author dutianze
+ * @date 2022/6/14
+ */
+public class N_538_ConvertBSTToGreaterTree {
+}
