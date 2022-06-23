e3a9b7d3326970b21d249b7497797d513d5929bc
dutianze <dutianzer@gmail.com>
2022-06-23 23:53:03 +0800

    SelectionSort

---
+++ algs/src/main/java/com/dutianze/algs/leetcode/sort/SelectionSort.java
@@ -0,0 +1,26 @@
+package com.dutianze.algs.leetcode.sort;
+
+/**
+ * @author dutianze
+ * @date 2022/6/23
+ */
+public class SelectionSort implements Sort {
+
+    @Override
+    public int[] sort(int[] array) {
+        if (array == null) {
+            return null;
+        }
+        final int N = array.length;
+        for (int i = 0; i < N; i++) {
+            int swapIndex = i;
+            for (int j = i + 1; j < N; j++) {
+                if (array[j] < array[swapIndex]) {
+                    swapIndex = j;
+                }
+            }
+            swap(array, i, swapIndex);
+        }
+        return array;
+    }
+}
