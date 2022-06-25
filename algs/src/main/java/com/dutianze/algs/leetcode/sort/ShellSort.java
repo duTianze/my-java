dac8133378007d389354640f6765a178e81f87d8
dutianze <dutianzer@gmail.com>
2022-06-25 23:52:54 +0800

    ShellSort

---
+++ algs/src/main/java/com/dutianze/algs/leetcode/sort/ShellSort.java
@@ -0,0 +1,27 @@
+package com.dutianze.algs.leetcode.sort;
+
+/**
+ * @author dutianze
+ * @date 2022/6/25
+ */
+public class ShellSort implements Sort {
+
+    @Override
+    public int[] sort(int[] array) {
+        int len = array.length;
+        int temp, gap = len / 2;
+        while (gap > 0) {
+            for (int i = gap; i < len; i++) {
+                temp = array[i];
+                int preIndex = i - gap;
+                while (preIndex >= 0 && array[preIndex] > temp) {
+                    array[preIndex + gap] = array[preIndex];
+                    preIndex -= gap;
+                }
+                array[preIndex + gap] = temp;
+            }
+            gap /= 2;
+        }
+        return array;
+    }
+}
