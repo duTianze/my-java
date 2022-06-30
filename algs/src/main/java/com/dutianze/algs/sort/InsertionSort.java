ed83065250dc456f3b6ce1867d618c73809f335e
dutianze <dutianzer@gmail.com>
2022-07-01 01:31:37 +0800

    InsertionSort

--- algs/src/main/java/com/dutianze/algs/sort/InsertionSort.java
+++ algs/src/main/java/com/dutianze/algs/sort/InsertionSort.java
@@ -5,15 +5,25 @@ package com.dutianze.algs.sort;
  * @date 2022/6/24
  */
 public class InsertionSort implements Sort {
+
     @Override
     public void sort(int[] arr) {
         if (isEmpty(arr)) {
             return;
         }
         for (int i = 1; i < arr.length; i++) {
-            for (int j = i; j > 0 && arr[j] < arr[j - 1]; j--) {
-                swap(arr, j - 1, j);
-            }
+            insert(arr, i);
         }
     }
+
+    private void insert(int[] arr, int n) {
+        int k = arr[n];
+        int i = n;
+        while (i > 0 && arr[i - 1] > k) {
+            arr[i] = arr[i - 1];
+            i--;
+        }
+        arr[i] = k;
+    }
+
 }
