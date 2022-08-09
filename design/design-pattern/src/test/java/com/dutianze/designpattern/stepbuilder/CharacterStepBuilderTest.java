f497c412e41e2114f9cb58ad97c7a315099a595b
dutianze <dutianzer@gmail.com>
2022-08-09 09:51:01 +0800

    stepbuilder

---
+++ design/design-pattern/src/test/java/com/dutianze/designpattern/stepbuilder/CharacterStepBuilderTest.java
@@ -0,0 +1,39 @@
+package com.dutianze.designpattern.stepbuilder;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author dutianze
+ * @date 2022/8/9
+ */
+class CharacterStepBuilderTest {
+
+    @Test
+    void testBuildWizard() {
+        final Character character = CharacterStepBuilder.newBuilder()
+                                                        .name("Merlin")
+                                                        .wizardClass("alchemist")
+                                                        .withSpell("poison")
+                                                        .withAbility("invisibility")
+                                                        .withAbility("wisdom")
+                                                        .noMoreAbilities()
+                                                        .build();
+
+        assertEquals("Merlin", character.getName());
+        assertEquals("alchemist", character.getWizardClass());
+        assertEquals("poison", character.getSpell());
+        assertNotNull(character.toString());
+
+        final List<String> abilities = character.getAbilities();
+        assertNotNull(abilities);
+        assertEquals(2, abilities.size());
+        assertTrue(abilities.contains("invisibility"));
+        assertTrue(abilities.contains("wisdom"));
+
+    }
+
+}
