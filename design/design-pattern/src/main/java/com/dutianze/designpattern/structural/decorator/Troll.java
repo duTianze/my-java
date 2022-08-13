acecef754973a4b380022a8479f526cf491be260
dutianze <dutianzer@gmail.com>
2022-08-13 13:17:40 +0800

    fix

--- design/design-pattern/src/main/java/com/dutianze/designpattern/structural/decorator/Troll.java
+++ design/design-pattern/src/main/java/com/dutianze/designpattern/structural/decorator/Troll.java
@@ -1,6 +1,15 @@
 package com.dutianze.designpattern.structural.decorator;
 
 /**
+ * <h2 id="known-uses">Known uses</h2>
+ * <ul>
+ * <li><a href="http://docs.oracle.com/javase/8/docs/api/java/io/InputStream.html">java.io.InputStream</a>, <a href="http://docs.oracle.com/javase/8/docs/api/java/io/OutputStream.html">java.io.OutputStream</a>,
+ * <a href="http://docs.oracle.com/javase/8/docs/api/java/io/Reader.html">java.io.Reader</a> and <a href="http://docs.oracle.com/javase/8/docs/api/java/io/Writer.html">java.io.Writer</a></li>
+ * <li><a href="http://docs.oracle.com/javase/8/docs/api/java/util/Collections.html#synchronizedCollection-java.util.Collection-">java.util.Collections#synchronizedXXX()</a></li>
+ * <li><a href="http://docs.oracle.com/javase/8/docs/api/java/util/Collections.html#unmodifiableCollection-java.util.Collection-">java.util.Collections#unmodifiableXXX()</a></li>
+ * <li><a href="http://docs.oracle.com/javase/8/docs/api/java/util/Collections.html#checkedCollection-java.util.Collection-java.lang.Class-">java.util.Collections#checkedXXX()</a></li>
+ * </ul>
+ *
  * @author dutianze
  * @date 2022/8/13
  */
