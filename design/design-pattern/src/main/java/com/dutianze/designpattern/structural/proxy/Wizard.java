e3f842239bd6460d223ce1d2bd34a53874f0ac21
dutianze <dutianzer@gmail.com>
2022-08-13 15:50:42 +0800

    fix

--- design/design-pattern/src/main/java/com/dutianze/designpattern/structural/proxy/Wizard.java
+++ design/design-pattern/src/main/java/com/dutianze/designpattern/structural/proxy/Wizard.java
@@ -3,6 +3,14 @@ package com.dutianze.designpattern.structural.proxy;
 import lombok.RequiredArgsConstructor;
 
 /**
+ * <h2 id="known-uses">Known uses</h2>
+ * <ul>
+ * <li><a href="http://docs.oracle.com/javase/8/docs/api/java/lang/reflect/Proxy.html">java.lang.reflect.Proxy</a></li>
+ * <li><a href="https://commons.apache.org/proper/commons-proxy/">Apache Commons Proxy</a></li>
+ * <li>Mocking frameworks <a href="https://site.mockito.org/">Mockito</a>,
+ * <a href="https://powermock.github.io/">Powermock</a>, <a href="https://easymock.org/">EasyMock</a></li>
+ * </ul>
+ *
  * @author dutianze
  * @date 2022/8/13
  */
