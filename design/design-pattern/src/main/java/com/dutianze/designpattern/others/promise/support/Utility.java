c5927fd460700e0fae578bb32f33c4fba9fcf7d2
dutianze <dutianzer@gmail.com>
2022-08-31 09:24:29 +0800

    promise

---
+++ design/design-pattern/src/main/java/com/dutianze/designpattern/others/promise/support/Utility.java
@@ -0,0 +1,65 @@
+package com.dutianze.designpattern.others.promise.support;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.*;
+import java.net.URL;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+/**
+ * @author dutianze
+ * @date 2022/8/31
+ */
+@Slf4j
+public class Utility {
+
+    public static Map<Character, Long> characterFrequency(String fileLocation) {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileLocation))) {
+            return bufferedReader.lines()
+                                 .flatMapToInt(String::chars)
+                                 .mapToObj(x -> (char) x)
+                                 .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return Collections.emptyMap();
+    }
+
+    public static Character lowestFrequencyChar(Map<Character, Long> charFrequency) {
+        return charFrequency
+                .entrySet()
+                .stream()
+                .min(Comparator.comparingLong(Map.Entry::getValue))
+                .map(Map.Entry::getKey)
+                .orElseThrow();
+    }
+
+    public static Integer countLines(String fileLocation) {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(fileLocation))) {
+            return (int) bufferedReader.lines().count();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return 0;
+    }
+
+    public static String downloadFile(String urlString) throws IOException {
+        log.info("Downloading contents from url: {}", urlString);
+        URL url = new URL(urlString);
+        File file = File.createTempFile("promise_pattern", null);
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(url.openStream()));
+             FileWriter writer = new FileWriter(file)) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                writer.write(line);
+                writer.write("\n");
+            }
+            log.info("File downloaded at: {}", file.getAbsolutePath());
+            return file.getAbsolutePath();
+        }
+    }
+}
---
+++ design/design-pattern/src/test/java/com/dutianze/designpattern/others/promise/PromiseTest.java
@@ -0,0 +1,124 @@
+package com.dutianze.designpattern.others.promise;
+
+import com.dutianze.designpattern.others.promise.support.Utility;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.*;
+import java.util.function.Consumer;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+/**
+ * @author dutianze
+ * @date 2022/8/31
+ */
+@Slf4j
+class PromiseTest {
+
+    private static final String DEFAULT_URL =
+            "https://raw.githubusercontent.com/iluwatar/java-design-patterns/master/promise/README.md";
+    private static final CountDownLatch stopLatch = new CountDownLatch(2);
+    private static final ExecutorService executor = Executors.newFixedThreadPool(2);
+
+    private Promise<Integer> promise;
+
+    @BeforeEach
+    public void setUp() {
+        promise = new Promise<>();
+    }
+
+    @Test
+    void usage() {
+        assertDoesNotThrow(() -> {
+            download(DEFAULT_URL)
+                    .thenApply(Utility::countLines)
+                    .thenAccept(
+                            count -> {
+                                log.info("Line count is: {}", count);
+                                stopLatch.countDown();
+                            }
+                    );
+            download(DEFAULT_URL)
+                    .thenApply(Utility::characterFrequency)
+                    .thenApply(Utility::lowestFrequencyChar)
+                    .thenAccept(
+                            charFrequency -> {
+                                log.info("Char with lowest frequency is: {}", charFrequency);
+                                stopLatch.countDown();
+                            }
+                    );
+            stopLatch.await();
+            executor.shutdownNow();
+        });
+    }
+
+    @Test
+    void promiseIsFulfilledWithTheResultantValueOfExecutingTheTask() throws InterruptedException, ExecutionException {
+        promise.fulfillInAsync(new NumberCrunchingTask(), executor);
+
+        assertEquals(NumberCrunchingTask.CRUNCHED_NUMBER, promise.get());
+        assertTrue(promise.isDone());
+        assertFalse(promise.isCancelled());
+    }
+
+    @Test
+    void dependentPromiseIsFulfilledAfterTheConsumerConsumesTheResultOfThisPromise()
+            throws InterruptedException, ExecutionException {
+        Promise<Void> dependentPromise = promise
+                .fulfillInAsync(new NumberCrunchingTask(), executor)
+                .thenAccept(value -> assertEquals(NumberCrunchingTask.CRUNCHED_NUMBER, value));
+
+        dependentPromise.get();
+        assertTrue(dependentPromise.isDone());
+        assertFalse(dependentPromise.isCancelled());
+    }
+
+    @Test
+    void fetchingAnAlreadyFulfilledPromiseReturnsTheFulfilledValueImmediately() throws ExecutionException {
+        Promise<Integer> promise = new Promise<>();
+        promise.fulfill(NumberCrunchingTask.CRUNCHED_NUMBER);
+
+        Integer result = promise.get(1000, TimeUnit.SECONDS);
+        assertEquals(NumberCrunchingTask.CRUNCHED_NUMBER, result);
+    }
+
+    @SuppressWarnings({"unchecked", "rawtypes"})
+    @Test
+    void exceptionHandlerIsCalledWhenPromiseIsFulfilledExceptionally() {
+        Promise<Object> promise = new Promise<>();
+        Consumer exceptionHandler = mock(Consumer.class);
+        promise.onError(exceptionHandler);
+
+        Exception exception = new Exception("barf!");
+        promise.fulfillExceptionally(exception);
+
+        verify(exceptionHandler).accept(eq(exception));
+    }
+
+    private Promise<String> download(String urlString) {
+        return new Promise<String>()
+                .fulfillInAsync(() -> Utility.downloadFile(urlString), executor)
+                .onError(throwable -> {
+                             throwable.printStackTrace();
+                             stopLatch.countDown();
+                         }
+                );
+    }
+
+    private static class NumberCrunchingTask implements Callable<Integer> {
+
+        private static final Integer CRUNCHED_NUMBER = Integer.MAX_VALUE;
+
+        @Override
+        public Integer call() throws Exception {
+            // Do number crunching
+            Thread.sleep(100);
+            return CRUNCHED_NUMBER;
+        }
+    }
+}
