54d7b4204563f762c0c66cef50a8378ec13b17b9
dutianze <dutianzer@gmail.com>
2022-09-08 00:22:00 +0800

    add infixToPostfix

--- design/design-pattern/src/test/java/com/dutianze/designpattern/behavioral/interpreter/ExpressionTest.java
+++ design/design-pattern/src/test/java/com/dutianze/designpattern/behavioral/interpreter/ExpressionTest.java
@@ -2,6 +2,8 @@ package com.dutianze.designpattern.behavioral.interpreter;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Stack;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
@@ -11,11 +13,53 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class ExpressionTest {
 
     @Test
-    void testExpress() {
-        final String tokenString = "4 3 2 - 1 + *";
+    void testExpress1() {
+        final String infixExpression = "( ( 1 + 2 ) *  2 )";
+        String tokenString = infixToPostfix(infixExpression);
 
         int result = Expression.interpret(tokenString);
+        assertEquals(6, result);
+    }
 
-        assertEquals(8, result);
+    @Test
+    void testExpress2() {
+        final String infixExpression = "( ( 1 * 2 ) +  5 )";
+        String tokenString = infixToPostfix(infixExpression);
+
+        int result = Expression.interpret(tokenString);
+        assertEquals(7, result);
+    }
+
+    private static String infixToPostfix(String infixExpression) {
+        Stack<String> operands = new Stack<>();
+        Stack<String> operators = new Stack<>();
+
+        String[] inputValues = infixExpression.split("\\s+");
+
+        for (String value : inputValues) {
+            switch (value) {
+                case "(":
+                    //do nothing
+                    break;
+                case "+":
+                case "-":
+                case "*":
+                case "/":
+                    operators.push(value);
+                    break;
+                case ")":
+                    String value2 = operands.pop();
+                    String value1 = operands.pop();
+                    String operator = operators.pop();
+
+                    String newExpression = value1 + " " + value2 + " " + operator;
+                    operands.push(newExpression);
+                    break;
+                default:
+                    operands.push(value);
+                    break;
+            }
+        }
+        return operands.pop();
     }
 }
