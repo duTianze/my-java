294a20a723cf305c7721d6a35720e77e78d08474
dutianze <dutianzer@gmail.com>
2022-11-13 16:54:28 +0800

    file create

--- base/base-io/src/test/java/com/dutianze/io/FileCrudTest.java
+++ base/base-io/src/test/java/com/dutianze/io/FileCrudTest.java
@@ -1,10 +1,16 @@
 package com.dutianze.io;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import java.io.BufferedWriter;
 import java.io.File;
-import java.io.IOException;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -12,19 +18,56 @@ import org.junit.jupiter.api.Test;
  * @author dutianze
  * @date 2022/11/12
  */
+@Slf4j
 class FileCrudTest {
 
-  private final String FILE_NAME = "src/test/resources/fileToCreate.txt";
+  private final String FILE_DIR = "src/test/resources/fileToCreate.txt";
+  private final String CONTENT = "content";
 
   @BeforeEach
   public void cleanUpFiles() {
-    File targetFile = new File(FILE_NAME);
-    targetFile.delete();
+    Path.of(FILE_DIR).toFile().delete();
   }
 
   @Test
-  void createFile() throws IOException {
-    Path newFilePath = Paths.get(FILE_NAME);
-    Files.createFile(newFilePath);
+  void createFileByFile() {
+    // java 1.2
+    assertDoesNotThrow(() -> {
+      File file = new File(FILE_DIR);
+      log.info(file.createNewFile() ? "File is created!" : "File already exists.");
+      // Write to file
+      try (FileWriter writer = new FileWriter(file)) {
+        writer.write(CONTENT);
+      }
+    });
+  }
+
+  @Test
+  void createFileByFiles() {
+    // java 1.7
+    assertDoesNotThrow(() -> {
+      Path path = Paths.get(FILE_DIR);
+      try (BufferedWriter writer = Files.newBufferedWriter(path, StandardCharsets.UTF_8)) {
+        writer.write(CONTENT);
+      }
+    });
+  }
+
+  @Test
+  void createFileByPrintWriter() {
+    // java 10
+    assertDoesNotThrow(() -> {
+      try (PrintWriter writer = new PrintWriter(FILE_DIR, StandardCharsets.UTF_8)) {
+        writer.println(CONTENT);
+      }
+    });
+  }
+
+  @Test
+  void createFileByFiles2() {
+    // java 11
+    assertDoesNotThrow(() -> {
+      Files.writeString(Paths.get(FILE_DIR), CONTENT);
+    });
   }
 }
