4116c07e1f3482432a1910593c3c6bc0768a2c4a
dutianze <dutianzer@gmail.com>
2022-11-14 18:55:46 +0800

    file read

--- base/base-io/src/test/java/com/dutianze/io/FileReadTest.java
+++ base/base-io/src/test/java/com/dutianze/io/FileReadTest.java
@@ -2,9 +2,14 @@ package com.dutianze.io;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
+import java.util.Scanner;
 import java.util.stream.Stream;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
@@ -18,6 +23,17 @@ public class FileReadTest {
 
   private final Path FILE_PATH = Paths.get("src/test/resources/fileRead.txt");
 
+  @Test
+  void readFileJava7() {
+    assertDoesNotThrow(() -> {
+      byte[] bytes = Files.readAllBytes(FILE_PATH);
+      String content = new String(bytes, StandardCharsets.UTF_8);
+      log.info(content);
+
+      List<String> lines = Files.readAllLines(FILE_PATH, StandardCharsets.UTF_8);
+      lines.forEach(log::info);
+    });
+  }
 
   @Test
   void readFileJava8() {
@@ -28,8 +44,6 @@ public class FileReadTest {
         lines.forEach(log::info);
       }
 
-      System.out.println();
-
       // parallel
       System.out.println("parallel");
       try (Stream<String> lines = Files.lines(FILE_PATH)) {
@@ -37,10 +51,49 @@ public class FileReadTest {
       }
 
       // preserve order
-      System.out.println("for parallel, preserve order");
+      System.out.println("preserve order");
       try (Stream<String> lines = Files.lines(FILE_PATH)) {
         lines.parallel().forEachOrdered(log::info);
       }
     });
   }
+
+  @Test
+  void readFileJava11() {
+    assertDoesNotThrow(() -> {
+      String content = Files.readString(FILE_PATH);
+      log.info(content);
+    });
+  }
+
+  @Test
+  void readFileBufferedReader() {
+    assertDoesNotThrow(() -> {
+      try (BufferedReader br = new BufferedReader(new FileReader(FILE_PATH.toFile()))) {
+        for (String line; (line = br.readLine()) != null; ) {
+          log.info(line);
+        }
+      }
+
+      try (BufferedReader br = Files.newBufferedReader(FILE_PATH)) {
+        String line;
+        while ((line = br.readLine()) != null) {
+          log.info(line);
+        }
+      }
+    });
+  }
+
+  @Test
+  void readFileScanner() {
+    assertDoesNotThrow(() -> {
+      try (Scanner sc = new Scanner(new FileReader(FILE_PATH.toFile()))) {
+        while (sc.hasNextLine()) {
+          String line = sc.nextLine();
+          log.info(line);
+        }
+      }
+    });
+  }
+
 }
